e2f63d1c9beed0eef487541597a1a084bb1e7c0f
PyATB4x <wasimfr90@gmail.com>
2024-08-15 16:15:46 +0530

    ADD

--- PyATB4xLearning/src/ex_08082024/Lab001.py
+++ PyATB4xLearning/src/ex_08082024/Lab001.py
@@ -1,4 +1,11 @@
 print("Welcome to the world of Python!")
 print(2+2)
 print(2-2)
+print(2*2)
+
+
+number = 3.14159265359
+
+formatted_number = f"{number:.4f}"
+print(formatted_number)
 
---
+++ PyATB4xLearning/src/ex_08082024/Lab002.py
@@ -0,0 +1,1 @@
+print("Hello Wasim! 2nd program!!!")
